4b6927256f59d2a3e117d2c8a4f5a5be0a744af8
WojtekP <wojtekpietraszuk@gmail.com>
2023-09-14 09:58:35 +0200

    Add ui divider component
    Reusable divier component

---
+++ libs/shared/ui/src/lib/divider/divider.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'e-commerce-divider',
+  template: `<div
+    class="bg-background-700"
+    [ngClass]="{
+      'w-full h-[0.05rem]': position === 'horizontal',
+      'h-20 w-[0.05rem]': position === 'vertical'
+    }"
+  ></div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule],
+})
+export class DividerComponent {
+  @Input() position: 'vertical' | 'horizontal' = 'horizontal';
+}
