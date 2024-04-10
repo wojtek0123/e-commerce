f2f8f2488a9c00c46bb6be4da4658e71fce2cb64
Wojtek <wojtekpietraszuk@gmail.com>
2024-04-10 18:58:24 +0200

    Use shellConfig to get session on app initialization

--- apps/client-web-app/src/app/app.config.ts
+++ apps/client-web-app/src/app/app.config.ts
@@ -8,9 +8,11 @@ import {
 import { clientWebAppShellRoutes } from '@e-commerce/client-web-app/shell/feature';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { shellConfig } from '@e-commerce/client-web-app/shell/feature';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    ...shellConfig.providers,
     provideStoreDevtools({ logOnly: !isDevMode(), connectInZone: true }),
     provideHttpClient(),
     provideStore(),
