abfd629fb015de3cdd3856b451400d452bb69413
Wojtek <wojtekpietraszuk@gmail.com>
2024-04-15 19:17:23 +0200

    Add auth guard and remove supabase package
    Create auth guard to check whether the user is log in or nor and remove unnecessary package which generate weird errors in console even if the package is not used anywhere

--- libs/shared/data-access/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://yvvxjwoqcsvhrocqgmje.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dnhqd29xY3N2aHJvY3FnbWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk4NzE5OTgsImV4cCI6MjAwNTQ0Nzk5OH0.MvKn3BbnST-gFv_nEIo3o4Pp90KeibSKiTjCHeDhhTk';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
