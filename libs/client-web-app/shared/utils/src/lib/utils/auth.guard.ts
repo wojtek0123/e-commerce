5e4fb3ce382557327aa1068c64385e4c3624ab87
Wojtek <wojtekpietraszuk@gmail.com>
2024-04-17 15:50:50 +0200

    Rewrite guard to use local storage
    The data-access dependency is not allowed in local storage that's why I start getting tokens from local storage

--- libs/client-web-app/shared/utils/src/lib/utils/auth.guard.ts
+++ libs/client-web-app/shared/utils/src/lib/utils/auth.guard.ts
@@ -5,22 +5,25 @@ import {
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { authSelectors } from '@e-commerce/client-web-app/shared/data-access/auth';
-import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (
   _next: ActivatedRouteSnapshot,
-  _state: RouterStateSnapshot
+  state: RouterStateSnapshot
 ) => {
-  const store = inject(Store);
   const router = inject(Router);
 
-  return store.select(authSelectors.selectTokens).pipe(
-    map((tokens) => !!tokens),
-    tap((hasAccess) => {
-      if (!hasAccess) router.navigate(['/auth/login']);
-      if (hasAccess && _state.url.includes('auth')) router.navigate(['/']);
-    })
-  );
+  const accessToken = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+
+  if (!accessToken || !refreshToken) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+
+  if (state.url.includes('auth')) {
+    router.navigate(['/']);
+    return false;
+  }
+
+  return true;
 };
