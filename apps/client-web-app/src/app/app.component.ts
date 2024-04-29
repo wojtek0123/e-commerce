0f3420f1d767c9a9f4d1e64930f5c41b5b1c6978
Wojtek <wojtekpietraszuk@gmail.com>
2024-04-29 07:34:14 +0200

    Add new lib, list all categories in menu
    Lib feature type to display books (filter, soriting and searching)

--- apps/client-web-app/src/app/app.component.ts
+++ apps/client-web-app/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,4 +8,4 @@ import { RouterOutlet } from '@angular/router';
   template: `<router-outlet />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent { }
+export class AppComponent {}
