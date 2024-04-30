18aa53863584e76c665731c4e93501ae1b51a871
Wojtek <wojtekpietraszuk@gmail.com>
2024-04-30 22:32:14 +0200

    Update styles in auth components

--- libs/client-web-app/auth/feature/shell/src/lib/shell/shell.component.ts
+++ libs/client-web-app/auth/feature/shell/src/lib/shell/shell.component.ts
@@ -7,7 +7,24 @@ import { TabMenuModule } from 'primeng/tabmenu';
   selector: 'e-commerce-shell',
   standalone: true,
   imports: [ButtonModule, TabMenuModule, NgIf],
-  templateUrl: './shell.component.html',
+  template: `
+    <div
+      class="flex flex-column-reverse gap-3 xl:grid xl:flex-row xl:mb-0 xl:gap-0 height-content"
+    >
+      <div class="xl:col-6 h-full">
+        <router-outlet></router-outlet>
+      </div>
+      <div
+        class="border-round overflow-hidden flex align-items-start h-full xl:col-6 xl:justify-content-end"
+      >
+        <img
+          class="border-round overflow-hidden object-fit-cover object-position-center w-full h-full"
+          src="../../assets/auth-books.jpg"
+          alt="book store"
+        />
+      </div>
+    </div>
+  `,
   styles: [
     `
       :host ::ng-deep input {
