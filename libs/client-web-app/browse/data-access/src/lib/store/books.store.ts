74b2779b4918442b817413491195fe09b3556b1a
Wojtek <wojtekpietraszuk@gmail.com>
2024-05-04 13:35:16 +0200

    Add filters in query params
    User can copy and paste the url the the filters will apply to find books

--- libs/client-web-app/browse/data-access/src/lib/store/books.store.ts
+++ libs/client-web-app/browse/data-access/src/lib/store/books.store.ts
@@ -11,6 +11,7 @@ import { pipe, switchMap, tap } from 'rxjs';
 import { inject } from '@angular/core';
 import { BooksApiService } from '../services/books-api.service';
 import { tapResponse } from '@ngrx/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 interface BooksFilters {
   title: string | null;
@@ -38,59 +39,84 @@ const initialBooksState: BooksState = {
 
 export const BooksStore = signalStore(
   withState(initialBooksState),
-  withMethods((store, booksApi = inject(BooksApiService)) => ({
-    getBooks: rxMethod<{ categoryIds?: number[]; tags?: BookTag[] }>(
-      pipe(
-        tap(() => patchState(store, { status: 'loading' })),
-        switchMap(({ tags, categoryIds }) =>
-          booksApi.getBooks$({ tagsIn: tags, categoryIds }).pipe(
-            tapResponse({
-              next: (books) => {
-                patchState(store, { status: 'ok', books });
-              },
-              error: (responseError: ResponseError) => {
-                patchState(store, {
-                  status: { error: responseError.error.message },
-                });
-              },
-            })
-          )
-        )
-      )
-    ),
-    filterBooks: rxMethod<void>(
-      pipe(
-        tap(() => patchState(store, { status: 'loading' })),
-        switchMap(() =>
-          booksApi
-            .getBooks$({
-              tagsIn: store.filters.tags() ?? undefined,
-              title: store.filters.title() ?? undefined,
-              categoryIds:
-                store.filters.categories()?.map((c) => c.id) ?? undefined,
-            })
-            .pipe(
+  withMethods(
+    (
+      store,
+      booksApi = inject(BooksApiService),
+      route = inject(ActivatedRoute),
+      router = inject(Router)
+    ) => ({
+      getBooks: rxMethod<{ categoryIds?: number[]; tags?: BookTag[] }>(
+        pipe(
+          tap(() => patchState(store, { status: 'loading' })),
+          switchMap(({ tags, categoryIds }) =>
+            booksApi.getBooks$({ tagsIn: tags, categoryIds }).pipe(
               tapResponse({
-                next: (books) => patchState(store, { status: 'ok', books }),
-                error: (responseError: ResponseError) =>
+                next: (books) => {
+                  patchState(store, { status: 'ok', books });
+                },
+                error: (responseError: ResponseError) => {
                   patchState(store, {
                     status: { error: responseError.error.message },
-                  }),
+                  });
+                },
               })
             )
+          )
         )
-      )
-    ),
-    setFilters: (filters: Partial<BooksFilters>) => {
-      patchState(store, { filters: { ...store.filters(), ...filters } });
+      ),
+      filterBooks: rxMethod<void>(
+        pipe(
+          tap(() => patchState(store, { status: 'loading' })),
+          switchMap(() =>
+            booksApi
+              .getBooks$({
+                tagsIn: store.filters.tags() ?? undefined,
+                title: store.filters.title() ?? undefined,
+                categoryIds:
+                  store.filters.categories()?.map((c) => c?.id) ?? undefined,
+              })
+              .pipe(
+                tapResponse({
+                  next: (books) => patchState(store, { status: 'ok', books }),
+                  error: (responseError: ResponseError) =>
+                    patchState(store, {
+                      status: { error: responseError.error.message },
+                    }),
+                })
+              )
+          )
+        )
+      ),
+      updateFilterCategories: (categories: Category[] | null) => {
+        patchState(store, (state) => ({
+          filters: { ...state.filters, categories },
+        }));
 
-      // const queryParams: Params = {};
+        const queryCats = categories
+          ?.map((c) => c?.name.toLowerCase().split(' ').join('_'))
+          .join(',');
 
-      // router.navigate([], {
-      //   relativeTo: route,
-      //   queryParams,
-      //   queryParamsHandling: 'merge',
-      // });
-    },
-  }))
+        router.navigate([], {
+          relativeTo: route,
+          queryParams: {
+            categories: queryCats || null,
+          },
+          queryParamsHandling: 'merge',
+        });
+      },
+      updateFilterTags: (tags: BookTag[] | null) => {
+        patchState(store, (state) => ({ filters: { ...state.filters, tags } }));
+
+        router.navigate([], {
+          relativeTo: route,
+          queryParams: { tags: tags?.join(',') || null },
+          queryParamsHandling: 'merge',
+        });
+      },
+      clearFilters: () => {
+        patchState(store, { filters: initialBooksState.filters });
+      },
+    })
+  )
 );
--- libs/client-web-app/browse/feature/books/src/lib/lib.routes.ts
+++ libs/client-web-app/browse/feature/books/src/lib/lib.routes.ts
@@ -4,11 +4,13 @@ import {
   BooksApiService,
   BooksStore,
 } from '@e-commerce/client-web-app/browse/data-access';
+import { categoriesResolver } from './resolvers/categories.resolver';
 
 export const booksRoutes: Route[] = [
   {
     path: '',
     component: FeatureBooksComponent,
+    resolve: { categories: categoriesResolver },
     providers: [BooksApiService, BooksStore],
   },
 ];
