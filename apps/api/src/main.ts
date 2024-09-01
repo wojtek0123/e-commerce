638fb667a78fabccd75e99b2888b356765ee6c64
Wojtek <wojtekpietraszuk@gmail.com>
2024-09-01 23:05:36 +0200

    Add service worker caching

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
   });
 
   const config = new DocumentBuilder()
