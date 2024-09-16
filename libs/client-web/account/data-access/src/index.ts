93c09c4f2ab6b7cc473b3dcdd5c163ee74a9e641
Wojtek <wojtekpietraszuk@gmail.com>
2024-09-16 18:35:31 +0200

    Standarize gap

--- libs/client-web/account/data-access/src/index.ts
+++ libs/client-web/account/data-access/src/index.ts
@@ -1,5 +1,1 @@
-export * from './lib/stores/orders/orders.state';
-export * from './lib/stores/orders/orders.reducer';
-export * from './lib/stores/orders/orders.actions';
-export * from './lib/stores/orders/orders.effects';
-export * as ordersSelectors from './lib/stores/orders/orders.selectors';
+export * from './lib/stores/orders/orders.store';
---
+++ libs/client-web/account/data-access/src/lib/stores/orders/orders.store.ts
@@ -0,0 +1,56 @@
+import { inject } from '@angular/core';
+import {
+  OrderDetails,
+  OrderDetailsApiService,
+  ResponseError,
+} from '@e-commerce/client-web/shared/data-access';
+import { tapResponse } from '@ngrx/operators';
+import {
+  patchState,
+  signalStore,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { pipe, switchMap, tap } from 'rxjs';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+
+interface OrdersState {
+  loading: boolean;
+  error: string | null;
+  orders: OrderDetails[];
+}
+
+const initialOrdersState: OrdersState = {
+  loading: false,
+  error: null,
+  orders: [],
+};
+
+export const OrdersStore = signalStore(
+  withState(initialOrdersState),
+  withMethods((store, ordersApi = inject(OrderDetailsApiService)) => ({
+    getOrders: rxMethod<void>(
+      pipe(
+        tap(() => patchState(store, { loading: true })),
+        switchMap(() =>
+          ordersApi.getMany().pipe(
+            tapResponse({
+              next: (orders) => {
+                patchState(store, { loading: false, orders });
+              },
+              error: (error: ResponseError) => {
+                patchState(store, { loading: false, error: error.message });
+              },
+            }),
+          ),
+        ),
+      ),
+    ),
+  })),
+  withHooks({
+    onInit(store) {
+      store.getOrders();
+    },
+  }),
+);
