3a9ed3273c9ae01699687886289e0d59830d5668
Wojtek <wojtekpietraszuk@gmail.com>
2024-10-14 20:25:54 +0200

    Rewrote styles to tailwind and added container queries to tailwind

--- libs/client-web/cart/ui/src/lib/cart-item-skeleton/cart-item-skeleton.component.ts
+++ libs/client-web/cart/ui/src/lib/cart-item-skeleton/cart-item-skeleton.component.ts
@@ -5,23 +5,38 @@ import { SkeletonModule } from 'primeng/skeleton';
   selector: 'lib-cart-item-sekeleton',
   template: `
     <div
-      class="flex w-full justify-content-between surface-hover p-3 border-round"
+      class="flex w-full items-center justify-between bg-content-background p-base rounded-base @container"
     >
-      <div class="flex gap-3">
-        <p-skeleton width="8rem" height="8rem" />
-        <div class="flex flex-column justify-content-between gap-3">
-          <p-skeleton width="20rem" height="3rem" />
-          <p-skeleton width="5.6rem" height="3rem" />
+      <div class="flex items-center gap-base w-full">
+        <p-skeleton height="8rem" class="max-w-32 w-full min-w-20" />
+        <div
+          class="flex flex-col justify-between gap-base w-full @6xl:grid @6xl:grid-cols-[1fr_0.7fr] @6xl:items-center"
+        >
+          <p-skeleton
+            width="100%"
+            height="3rem"
+            class="@2xl:max-w-full @6xl:max-w-80"
+          />
+          <p-skeleton
+            width="100%"
+            height="2.5rem"
+            class="@xl:max-w-[65%] @6xl:max-w-64"
+          />
         </div>
-      </div>
-      <div class="flex flex-column justify-content-between gap-3 mt-4">
-        <p-skeleton width="9rem" height="1.5rem" />
-        <p-skeleton width="8.75rem" height="3.125rem" />
+        <div
+          class="hidden flex-col justify-between items-end gap-base w-full @2xl:flex @4xl:flex-row @4xl:justify-end @4xl:items-center"
+        >
+          <p-skeleton width="9rem" height="2rem" />
+          <p-skeleton width="8.75rem" height="3rem" />
+        </div>
       </div>
     </div>
   `,
   standalone: true,
   imports: [SkeletonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-full',
+  },
 })
 export class CartItemSkeletonComponent {}
