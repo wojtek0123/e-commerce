e04fa0309fe44f0eb3316e7551f1413b65d759c3
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-10-30 20:09:29 +0100

    Added test case to auth shell component

--- libs/client-web/auth/feature/shell/src/lib/auth-shell.component.spec.ts
+++ libs/client-web/auth/feature/shell/src/lib/auth-shell.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ClientWebAuthShellComponent } from './client-web-auth-shell.component';
+import { AuthShellComponent } from './auth-shell.component';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
-describe('ClientWebAuthShellComponent', () => {
-  let component: ClientWebAuthShellComponent;
-  let fixture: ComponentFixture<ClientWebAuthShellComponent>;
+describe('AuthShellComponent', () => {
+  let component: AuthShellComponent;
+  let fixture: ComponentFixture<AuthShellComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClientWebAuthShellComponent]
+      imports: [AuthShellComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ClientWebAuthShellComponent);
+    fixture = TestBed.createComponent(AuthShellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the image with the correct src and alt attributes', () => {
+    const imgElement = fixture.debugElement.query(By.css('img'));
+
+    expect(imgElement).toBeTruthy();
+    expect(imgElement.attributes['src']).toBe('assets/auth-books.jpg');
+    expect(imgElement.attributes['alt']).toBe('book store');
+  });
+
+  it('should contain router-outlet for child routes', () => {
+    const routerOutlet = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(routerOutlet).toBeTruthy();
   });
 });
