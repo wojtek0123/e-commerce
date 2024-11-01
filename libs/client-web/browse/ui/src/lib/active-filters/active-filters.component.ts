cbd14a8e2aa967d53498a6a5ffb439e8316ae10a
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-11-01 09:04:46 +0100

    Fixed the height change of the active filters and testing auth guard

--- libs/client-web/browse/ui/src/lib/active-filters/active-filters.component.ts
+++ libs/client-web/browse/ui/src/lib/active-filters/active-filters.component.ts
@@ -7,11 +7,12 @@ import {
 import { ActiveFilter } from '@e-commerce/client-web/browse/data-access';
 import { ButtonModule } from 'primeng/button';
 import { ChipModule } from 'primeng/chip';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'lib-active-filters',
   standalone: true,
-  imports: [ButtonModule, ChipModule],
+  imports: [ButtonModule, ChipModule, TooltipModule],
   templateUrl: './active-filters.component.html',
   styleUrl: './active-filters.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
