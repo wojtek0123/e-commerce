c68011be0dc140a41b10eed573cf7dd1acb210d2
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-11-09 10:59:27 +0100

    Improved UX, do not disable buttons

--- libs/client-web/cart/feature/order-process/src/lib/guards/cart-items.guard.ts
+++ libs/client-web/cart/feature/order-process/src/lib/guards/cart-items.guard.ts
@@ -1,17 +1,32 @@
 import { inject } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { Router } from '@angular/router';
+import { CanMatchFn, Router } from '@angular/router';
 import { CartService } from '@e-commerce/client-web/cart/api';
+import { MessageService } from 'primeng/api';
 import { filter, map, skipUntil } from 'rxjs';
 
-export const cartItemsGuard = () => {
+export const cartItemsGuard: CanMatchFn = () => {
   const router = inject(Router);
   const cartService = inject(CartService);
+  const messageService = inject(MessageService);
   const itemsCount = cartService.itemsCount;
   const loading$ = toObservable(cartService.loading);
 
   return loading$.pipe(
     skipUntil(loading$.pipe(filter((loading) => !loading))),
-    map(() => (itemsCount() === 0 ? router.createUrlTree(['/browse']) : true)),
+    map(() => {
+      if (itemsCount() === 0) {
+        messageService.add({
+          severity: 'info',
+          detail:
+            "Your card is empty that's why you don't have access to checkout",
+          summary: 'No access',
+          life: 5000,
+        });
+        return router.createUrlTree(['/browse']);
+      } else {
+        return true;
+      }
+    }),
   );
 };
