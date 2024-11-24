9205627201f532523a3919a48c3fdcd01d8b75fc
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-11-24 11:05:56 +0100

    Added electron to admin-dashboard app

--- apps/admin-dashboard/vite.config.ts
+++ apps/admin-dashboard/vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import electron from 'vite-plugin-electron';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -14,11 +16,44 @@ export default defineConfig({
       allow: [searchForWorkspaceRoot(process.cwd())],
     },
   },
+  base: process.env.VITE_IS_ELECTRON_APP ? './' : '/',
+  clearScreen: true,
+  assetsInclude: /\.(pdf|jpg|png|svg)$/,
   preview: {
     port: 4300,
     host: 'localhost',
   },
-  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    vue(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    process.env.VITE_IS_ELECTRON_APP
+      ? electron([
+          {
+            entry: path.join(__dirname, 'electron/main.ts'),
+            vite: {
+              build: {
+                sourcemap: true,
+                outDir: '../../dist/apps/admin-dashboard/electron/main',
+              },
+            },
+          },
+
+          {
+            entry: path.join(__dirname, 'electron/preload.ts'),
+            vite: {
+              build: {
+                sourcemap: 'inline',
+                outDir: '../../dist/apps/admin-dashboard/electron/preload',
+              },
+            },
+            onstart(options) {
+              options.reload();
+            },
+          },
+        ])
+      : null,
+  ],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
