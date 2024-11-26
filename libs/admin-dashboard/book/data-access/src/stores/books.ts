87d32b33bf36d8ca90545c3dccdc2b108814ecb5
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-11-26 23:02:24 +0100

    Created form to add book

--- libs/admin-dashboard/book/data-access/src/stores/books.ts
+++ libs/admin-dashboard/book/data-access/src/stores/books.ts
@@ -1,33 +1,95 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { Book } from '@e-commerce/shared/api-models';
+import { Author, Book, Category } from '@e-commerce/shared/api-models';
+import axios, { AxiosError } from 'axios';
+import { Publisher } from '@prisma/client';
 
 export const useBooksStore = defineStore('books', () => {
   const books = ref<Book[]>([]);
   const loading = ref(false);
   const error = ref<string | null>(null);
 
+  const categories = ref<Category[]>([]);
+  const authors = ref<Author[]>([]);
+  const publishers = ref<Publisher[]>([]);
+
   async function getBooks() {
     loading.value = true;
 
     try {
-      const response = await fetch('http://localhost:3000/books', {
-        method: 'get',
-      });
-      const data = await response.json();
-
-      if (!response.ok) {
-        console.log(data);
-        throw new Error(data);
+      const response = await axios.get<{ items: Book[]; total: number }>(
+        'http://localhost:3000/books',
+      );
+      books.value = response.data.items;
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        error.value =
+          e.response?.data?.message ?? 'Error occurred while fetching books';
+      } else {
+        error.value = 'An unexpected error occurred';
       }
-
-      books.value = data.items;
-    } catch (error) {
-      // error.value = error?.['error']?.message;
     } finally {
       loading.value = false;
     }
   }
 
-  return { books, loading, error, getBooks };
+  async function getCategories(search: string | undefined) {
+    try {
+      const response = await axios.get<Category[]>(
+        `http://localhost:3000/categories`,
+        {
+          params: {
+            titleLike: search,
+          },
+        },
+      );
+
+      categories.value = response.data;
+    } catch (e: unknown) {
+      console.log(e);
+    }
+  }
+
+  async function getAuthors(search: string | undefined) {
+    try {
+      const response = await axios.get<Author[]>(
+        'http://localhost:3000/authors',
+        {
+          params: {
+            nameLike: search,
+          },
+        },
+      );
+
+      authors.value = response.data;
+    } catch (e: unknown) {
+      console.log(e);
+    }
+  }
+
+  async function getPublishers(search: string | undefined) {
+    try {
+      const response = await axios.get<Publisher[]>(
+        'http://localhost:3000/publishers',
+        { params: { nameLike: search } },
+      );
+
+      publishers.value = response.data;
+    } catch (e: unknown) {
+      console.log(e);
+    }
+  }
+
+  return {
+    books,
+    loading,
+    error,
+    getBooks,
+    getCategories,
+    categories,
+    getAuthors,
+    authors,
+    getPublishers,
+    publishers,
+  };
 });
--- libs/admin-dashboard/book/feature/book-list/src/lib/book-list.vue
+++ libs/admin-dashboard/book/feature/book-list/src/lib/book-list.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
 import Tag from 'primevue/tag';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Skeleton from 'primevue/skeleton';
+import InputText from 'primevue/inputtext';
 import { useBooksStore } from '@e-commerce/admin-dashboard/book/data-access';
 import { onMounted } from 'vue';
+import { ref } from 'vue';
+import { Book } from '@e-commerce/shared/api-models';
+
+import AddBookDrawer from './components/add-book-drawer/add-book-drawer.vue';
 
 const store = useBooksStore();
+
+const selectedBooks = ref<Book[]>([]);
+
+function retryGettingBooks() {
+  store.getBooks();
+}
 
 onMounted(() => {
   store.getBooks();
@@ -14,60 +26,95 @@ onMounted(() => {
 </script>
 
 <template>
-  <DataTable
-    :value="store.books"
-    :loading="store.loading"
-    table-class="rounded-base overflow-hidden min-w-[50rem]"
-  >
-    <Column field="id" header="ID">
-      <template #loading>
-        <div
-          class="flex items-center"
-          :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
-        >
-          <Skeleton width="60%" height="1rem" />
-        </div>
-      </template>
-    </Column>
-    <Column field="title" header="Title">
-      <template #loading>
-        <div
-          class="flex items-center"
-          :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
-        >
-          <Skeleton width="60%" height="1rem" />
-        </div>
-      </template>
-    </Column>
-    <Column field="tag" header="Tag">
-      <template #body="book">
-        <Tag :value="book.data.tag" severity="secondary"></Tag>
-      </template>
-      <template #loading>
-        <div
-          class="flex items-center"
-          :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
-        >
-          <Skeleton width="60%" height="1rem" />
-        </div>
-      </template>
-    </Column>
-    <Column field="authors" header="Authors">
-      <template #body="book">
-        <span :key="author.id" v-for="(author, index) of book.data.authors">
-          {{ author.name
-          }}{{ index !== book.data.authors.length - 1 ? ', ' : '' }}
-        </span>
-      </template>
+  <div class="flex flex-col gap-base">
+    <div
+      class="bg-content-background w-full p-4 rounded-base flex justify-between items-center"
+    >
+      <div class="flex items-center gap-base">
+        <AddBookDrawer />
+        <Button
+          severity="danger"
+          :outlined="true"
+          icon="pi pi-trash"
+          label="Delete"
+        ></Button>
+      </div>
 
-      <template #loading>
-        <div
-          class="flex items-center"
-          :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
-        >
-          <Skeleton width="60%" height="1rem" />
-        </div>
-      </template>
-    </Column>
-  </DataTable>
+      <InputText class="w-full max-w-[40rem]" />
+    </div>
+
+    <div v-if="store.error" class="flex flex-col items-center gap-4 mt-10">
+      <p class="text-5xl text-center">{{ store.error }}</p>
+      <p class="text-xl text-muted-color">
+        Unable to load books. Please try again.
+      </p>
+      <Button
+        label="Retry"
+        icon="pi pi-refresh"
+        @click="retryGettingBooks()"
+        severity="secondary"
+      ></Button>
+    </div>
+
+    <DataTable
+      v-else
+      v-model:selection="selectedBooks"
+      :value="store.books"
+      :loading="store.loading"
+      data-key="id"
+      table-class="rounded-base overflow-hidden min-w-[50rem]"
+    >
+      <Column selection-mode="multiple" header-class="w-12"></Column>
+      <Column field="id" header="ID">
+        <template #loading>
+          <div
+            class="flex items-center"
+            :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
+          >
+            <Skeleton width="60%" height="1rem" />
+          </div>
+        </template>
+      </Column>
+      <Column field="title" header="Title">
+        <template #loading>
+          <div
+            class="flex items-center"
+            :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
+          >
+            <Skeleton width="60%" height="1rem" />
+          </div>
+        </template>
+      </Column>
+      <Column field="tag" header="Tag">
+        <template #body="book">
+          <Tag :value="book.data.tag" severity="secondary"></Tag>
+        </template>
+        <template #loading>
+          <div
+            class="flex items-center"
+            :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
+          >
+            <Skeleton width="60%" height="1rem" />
+          </div>
+        </template>
+      </Column>
+      <Column field="authors" header="Authors">
+        <template #body="book">
+          <span :key="author.id" v-for="(author, index) of book.data.authors">
+            {{ author.name
+            }}{{ index !== book.data.authors.length - 1 ? ', ' : '' }}
+          </span>
+        </template>
+
+        <template #loading>
+          <div
+            class="flex items-center"
+            :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
+          >
+            <Skeleton width="60%" height="1rem" />
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+  </div>
 </template>
