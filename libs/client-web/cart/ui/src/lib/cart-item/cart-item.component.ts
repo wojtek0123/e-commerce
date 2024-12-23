3e47e566fe823cdc5a6c73486ab0802f99d2eaeb
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2024-12-23 21:59:31 +0100

    Change img to p-image in cart-item and change error message in grid-books

--- libs/client-web/cart/ui/src/lib/cart-item/cart-item.component.ts
+++ libs/client-web/cart/ui/src/lib/cart-item/cart-item.component.ts
@@ -6,12 +6,13 @@ import {
   output,
 } from '@angular/core';
 import { Book, CartItem, CartItemBase } from '@e-commerce/shared/api-models';
-import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { APP_ROUTE_PATHS_TOKEN } from '@e-commerce/client-web/shared/app-config';
+import { Image } from 'primeng/image';
 
 @Component({
   selector: 'lib-cart-item',
@@ -21,35 +22,34 @@ import { APP_ROUTE_PATHS_TOKEN } from '@e-commerce/client-web/shared/app-config'
     ButtonModule,
     InputNumberModule,
     FormsModule,
-    NgOptimizedImage,
     RouterLink,
+    Image,
   ],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartItemComponent {
-  public item = input.required<CartItemBase>();
-  protected readonly appRoutePaths = inject(APP_ROUTE_PATHS_TOKEN);
+  item = input.required<CartItemBase>();
+  appRoutePaths = inject(APP_ROUTE_PATHS_TOKEN);
 
-  public onUpdateQuantity = output<{
+  onUpdateQuantity = output<{
     quantity: CartItem['quantity'];
     book: Book;
   }>();
-  public onDelete = output<{ bookId: Book['id'] }>();
+  onDelete = output<{ bookId: Book['id'] }>();
 
-  public remove() {
+  remove() {
     this.onDelete.emit({ bookId: this.item().book.id });
   }
 
-  public increase() {
+  increase() {
     this.onUpdateQuantity.emit({
       quantity: this.item().quantity + 1,
       book: this.item().book,
     });
   }
 
-  public decrease() {
+  decrease() {
     const quantity = this.item().quantity - 1;
 
     if (quantity < 1) {
