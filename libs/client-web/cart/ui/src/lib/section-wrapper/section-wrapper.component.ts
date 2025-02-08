e5a33198e2f7874aaa9b23159690f0a87c416c9a
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-02-08 13:55:51 +0100

    Removed unnecessary files and made small improvements

--- libs/client-web/cart/ui/src/lib/section-wrapper/section-wrapper.component.ts
+++ libs/client-web/cart/ui/src/lib/section-wrapper/section-wrapper.component.ts
@@ -2,10 +2,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'lib-section-wrapper',
-  standalone: true,
-  imports: [],
   templateUrl: './section-wrapper.component.html',
-  styleUrl: './section-wrapper.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionWrapperComponent {
--- libs/client-web/cart/ui/src/lib/toggleable-content/toogleable-content.component.spec.ts
+++ libs/client-web/cart/ui/src/lib/toggleable-content/toogleable-content.component.spec.ts
@@ -1,9 +1,4 @@
-import {
-  ComponentFixture,
-  fakeAsync,
-  TestBed,
-  tick,
-} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToggleableContentComponent } from './toggleable-content.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
