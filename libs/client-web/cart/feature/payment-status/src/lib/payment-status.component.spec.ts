607841d16004e663d6bc96b245e81eccb5578e0f
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-02-09 13:49:46 +0100

    Wrote tests and made small changes
    Instead of ngClass use [class] and minor changes in text

--- libs/client-web/cart/feature/payment-status/src/lib/payment-status.component.spec.ts
+++ libs/client-web/cart/feature/payment-status/src/lib/payment-status.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaymentStatusComponent } from './payment-status.component';
+import { APP_ROUTE_PATHS_TOKEN } from '@e-commerce/client-web/shared/app-config';
+import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('PaymentStatusComponent', () => {
   let component: PaymentStatusComponent;
@@ -8,14 +11,40 @@ describe('PaymentStatusComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PaymentStatusComponent],
+      providers: [
+        {
+          provide: APP_ROUTE_PATHS_TOKEN,
+          useValue: { ORDERS: () => '/orders' },
+        },
+        provideRouter([]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PaymentStatusComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('orderDetailsId', 'order-id');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should loading lasts for 3s', async () => {
+    await fixture.whenStable();
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    expect(component.loading()).toBe(false);
+  });
+
+  it('should redirect to order details after clicking a link', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const href = fixture.debugElement
+      .query(By.css('a'))
+      .nativeElement.getAttribute('href');
+
+    expect(href).toEqual('/orders?orderDetailsId=order-id');
   });
 });
