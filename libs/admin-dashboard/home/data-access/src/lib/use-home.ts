f1c3af15f67d23e66aa32508ff728c833fbf658d
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-02-13 20:54:23 +0100

    Created default bar chart

--- libs/admin-dashboard/home/data-access/src/lib/use-home.ts
+++ libs/admin-dashboard/home/data-access/src/lib/use-home.ts
@@ -1,3 +1,52 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { OrderDetails } from '@e-commerce/shared/api-models';
+import axios, { AxiosError } from 'axios';
 
-export const useHomeStore = defineStore('home', () => {});
+export const useHomeStore = defineStore('home', () => {
+  const orders = ref<OrderDetails[]>([]);
+  const loading = ref(false);
+  const error = ref<string | null>(null);
+
+  const today = ref(new Date());
+  const priorDate = ref(
+    new Date(today.value.getTime() - 30 * 24 * 60 * 60 * 1000),
+  );
+
+  async function getOrders() {
+    loading.value = true;
+    orders.value = [];
+    error.value = null;
+
+    try {
+      const response = await axios.get<OrderDetails[]>(
+        `${import.meta.env.VITE_API_URL}/order-details`,
+        {
+          params: {
+            startDate: priorDate.value.toISOString(),
+            endDate: today.value.toISOString(),
+          },
+        },
+      );
+      orders.value = response.data;
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        error.value =
+          e.response?.data?.message ?? 'Error occurred while fetching books';
+      } else {
+        error.value = 'An unexpected error occurred';
+      }
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  return {
+    getOrders,
+    orders,
+    loading,
+    error,
+    today,
+    priorDate,
+  };
+});
--- libs/admin-dashboard/home/feature/home/src/lib/home.vue
+++ libs/admin-dashboard/home/feature/home/src/lib/home.vue
@@ -1,5 +1,93 @@
-<script lang="ts"></script>
+<script lang="ts" setup>
+import { useHomeStore } from '@e-commerce/admin-dashboard/home/data-access';
+import { onMounted, computed, ref } from 'vue';
+import BarChart from './components/BarChart.vue';
+import Stat from './components/Stat.vue';
+
+const store = useHomeStore();
+
+// const today = ref(new Date());
+// const priorDate = ref(
+//   new Date(today.value.getTime() - 30 * 24 * 60 * 60 * 1000),
+// );
+
+const groupedOrders = computed(() => {
+  // Create an array of the last 30 days
+  const dates: string[] = [];
+  for (let i = 0; i < 30; i++) {
+    const date = new Date(store.priorDate.getTime() + i * 24 * 60 * 60 * 1000);
+    dates.push(date.toISOString().split('T')[0]);
+  }
+
+  const initialDates = dates.reduce(
+    (acc, date) => {
+      acc[date] = 0;
+      return acc;
+    },
+    {} as Record<string, number>,
+  );
+
+  return store.orders.reduce((acc, order) => {
+    const orderDate = new Date(order.createdAt);
+    const date = orderDate.toISOString().split('T')[0];
+
+    if (orderDate >= store.priorDate && orderDate <= store.today) {
+      acc[date] = (acc[date] || 0) + 1;
+    }
+    return acc;
+  }, initialDates);
+});
+
+const chartData = computed(() => {
+  const dates = Object.keys(groupedOrders.value).sort();
+  return {
+    labels: dates.map((date) => {
+      const d = new Date(date);
+      return (
+        d.getDate() + ' ' + d.toLocaleString('default', { month: 'short' })
+      );
+    }),
+    datasets: [
+      {
+        label: 'Orders per day',
+        data: dates.map((date) => groupedOrders.value[date]),
+        backgroundColor: '#34d399',
+        borderColor: '#34d399',
+        borderWidth: 0,
+      },
+    ],
+  };
+});
+
+onMounted(() => {
+  store.getOrders();
+});
+</script>
 
 <template>
-  <div>Home</div>
+  <div class="@container flex flex-col gap-base">
+    <div class="grid grid-cols-2 @3xl:grid-cols-3 @6xl:grid-cols-6 gap-base">
+      <Stat header="Total revenue" :value="3000" />
+      <Stat header="Total orders" :value="2423" />
+      <Stat header="Total available books" :value="252" />
+      <Stat header="Total revenue" :value="123400" />
+      <Stat header="Total revenue" :value="12320" />
+      <Stat header="New customer" :value="5434" />
+    </div>
+    <div class="grid grid-cols-1 @3xl:grid-cols-2 gap-base">
+      <BarChart
+        v-if="!store.loading && store.orders.length > 0"
+        class="w-full"
+        :labels="chartData.labels"
+        :dataset="chartData.datasets"
+      />
+
+      <BarChart
+        v-if="!store.loading && store.orders.length > 0"
+        class="w-full"
+        :labels="chartData.labels"
+        :dataset="chartData.datasets"
+      />
+    </div>
+  </div>
 </template>
