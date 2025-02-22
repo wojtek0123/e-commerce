03744450af2485c4598b0c93d3e735e7e34c64d4
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-02-22 10:30:53 +0100

    Fixed button height in add-book-drawer and login page on mobile

--- libs/admin-dashboard/auth/feature/shell/src/lib/shell.vue
+++ libs/admin-dashboard/auth/feature/shell/src/lib/shell.vue
@@ -3,16 +3,18 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div class="flex flex-col-reverse gap-4 xl:grid xl:h-content xl:grid-cols-2">
-    <div class="flex flex-col items-center justify-center w-full">
+  <div
+    class="flex flex-col-reverse w-full gap-4 xl:grid xl:h-content xl:grid-cols-2"
+  >
+    <div class="flex flex-col items-center h-content justify-center w-full">
       <div
-        class="max-w-[30rem] flex flex-col items-center justify-center w-full"
+        class="max-w-[30rem] h-full flex flex-col items-center justify-center w-full"
       >
         <RouterView />
       </div>
     </div>
     <div
-      class="rounded-base overflow-hidden flex w-full h-full xl:content-end xl:h-content"
+      class="hidden xl:flex rounded-base overflow-hidden w-full h-full xl:content-end xl:h-content"
     >
       <img
         class="object-fill object-center w-full"
