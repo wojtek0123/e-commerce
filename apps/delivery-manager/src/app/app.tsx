4ef645f03da4be5f4df607d5282ff9bff1e2a718
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-03-08 09:15:26 +0100

    Implemented auth in delivery manager

--- apps/delivery-manager/src/app/app.tsx
+++ apps/delivery-manager/src/app/app.tsx
@@ -1,22 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import { DeliverManagerAuthFeatureShell } from '@e-commerce/delivery-manager/auth/feature/shell';
 import { PrimeReactProvider } from 'primereact/api';
-// import 'primereact/resources/themes/soho-dark/theme.css';
-
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
 import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.css'; // core css
 
 export function App() {
   return (
     <PrimeReactProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={<div>This is the generated root route. </div>}
-        />
-        <Route path="auth/*" element={<DeliverManagerAuthFeatureShell />} />
-      </Routes>
+      <div className="flex flex-col-reverse w-full h-svh xl:flex-row xl:p-gap xl:min-h-svh xl:h-full">
+        <Routes>
+          <Route
+            path="/"
+            element={<div>This is the generated root route. </div>}
+          />
+          <Route path="auth/*" element={<DeliverManagerAuthFeatureShell />} />
+        </Routes>
+      </div>
     </PrimeReactProvider>
   );
 }
--- apps/delivery-manager/src/main.tsx
+++ apps/delivery-manager/src/main.tsx
@@ -2,6 +2,68 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import axios from 'axios';
+import { Tokens } from '@e-commerce/shared/api-models';
+
+axios.interceptors.request.use(
+  function (config) {
+    // const authService = useAuthService();
+
+    // const { access, refresh } = authService.tokens.value;
+
+    config.headers.set('app', 'delivery-manager');
+
+    // config.headers.Authorization = `Bearer ${config.url?.includes('refresh') ? refresh : access}`;
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    const originalConfig = error.config;
+
+    if (
+      !originalConfig._retry &&
+      !originalConfig.url.includes('login') &&
+      error.response?.status === 401
+    ) {
+      originalConfig._retry = true;
+
+      // const authStore = useAuthStore();
+
+      try {
+        const { data } = await axios.post<{
+          accessToken: Tokens['accessToken'];
+          refreshToken: Tokens['refreshToken'];
+        }>(`${import.meta.env.VITE_API_URL}/auth/refresh`, {
+          // userId: authStore.userId,
+          // refreshToken: authStore.tokens.refresh,
+        });
+
+        // authStore.setSessionToStorage(data);
+
+        return axios.request({
+          ...originalConfig,
+          headers: {
+            ...originalConfig.headers,
+            Authorization: `Bearer ${data.accessToken}`,
+          },
+        });
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
