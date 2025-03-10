9fde2cbfe0eefa0d95ae6d97555bb61673caf167
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-03-10 18:39:56 +0100

    Created nav component

--- libs/delivery-manager/auth/api/src/lib/auth-api.ts
+++ libs/delivery-manager/auth/api/src/lib/auth-api.ts
@@ -2,11 +2,18 @@ import { useAuthStore } from '@e-commerce/delivery-manager/auth/data-access';
 
 export function useAuthApi() {
   const {
+    logout,
+    saveSessionToStorage,
     userId,
     tokens: { access, refresh },
   } = useAuthStore();
 
   return {
+    accessToken: access,
+    refreshToken: refresh,
+    userId,
     isAuthenticated: userId && access && refresh,
+    logout,
+    saveSessionToStorage,
   };
 }
---
+++ libs/delivery-manager/core/data-access/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'delivery-manager-core-data-access',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../coverage/libs/delivery-manager/core/data-access',
+};
