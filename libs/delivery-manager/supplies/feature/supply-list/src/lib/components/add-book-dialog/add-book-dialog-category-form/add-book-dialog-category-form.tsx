1c84ffe1c387775df41178b2868094c96ca11bd2
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-05-07 18:45:23 +0200

    Moved inventory form to another file

--- libs/delivery-manager/supplies/feature/supply-list/src/lib/components/add-book-dialog/add-book-dialog-category-form/add-book-dialog-category-form.tsx
+++ libs/delivery-manager/supplies/feature/supply-list/src/lib/components/add-book-dialog/add-book-dialog-category-form/add-book-dialog-category-form.tsx
@@ -1,10 +1,10 @@
 import { useToastStore } from '@e-commerce/delivery-manager/shared/data-access';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Category } from '@e-commerce/shared/api-models';
+import { Category, Paginated } from '@e-commerce/shared/api-models';
 
 type AddCategoryBody = {
   name: string;
@@ -37,6 +37,17 @@ export default function AddBookDialogCategoryForm({
     },
   });
 
+  const { data: categories } = useQuery<Category[]>({
+    queryKey: ['categories'],
+    queryFn: async () => {
+      const response = await axios.get<Paginated<Category>>(
+        `${import.meta.env.VITE_API_URL}/categories`,
+      );
+
+      return response.data.items;
+    },
+  });
+
   const onAddCategory: SubmitHandler<{ name: string }> = (state) => {
     const formErrors = Object.values(errors);
 
@@ -53,13 +64,24 @@ export default function AddBookDialogCategoryForm({
       <div className="flex flex-col gap-1">
         <label htmlFor="name">What is the category name?</label>
         <InputText
-          {...register('name', { required: true })}
+          {...register('name', {
+            required: true,
+            validate: (value) =>
+              !categories?.find((c) =>
+                c.name.toLowerCase().localeCompare(value.toLowerCase()),
+              ),
+          })}
           placeholder="Enter name"
           id="name"
           invalid={!!errors.name}
         />
-        {errors.name && (
+        {errors.name?.type === 'required' && (
           <span className="text-red-300 text-base">Name is required</span>
+        )}
+        {errors.name?.type === 'validate' && (
+          <span className="text-red-300 text-base">
+            This category is already added
+          </span>
         )}
       </div>
       <div className="grid grid-cols-2 gap-base mt-4">
